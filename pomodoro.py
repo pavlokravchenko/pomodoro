0177d7b99cf534cad7e68b5084660f1fb4ebb89b
pavlokravchenko <kravchenko@lab3d.com>
2022-06-03 16:39:41 +0200

    Added some expalanatory and todo comments.

--- pomodoro.py
+++ pomodoro.py
@@ -4,22 +4,27 @@ from win10toast import ToastNotifier
 
 notifier = ToastNotifier()
 
+# TODO: A way to change time values preferences
 WORK_TIME = 25 * 60
 PAUSE_TIME = 5 * 60
 
+# TODO: A multilang system
 NOTIF_TITLE = "Pomodoro"
 NOTIF_WORK_TEXT = "Lavoro"
 NOTIF_PAUSE_TEXT = "Pausa"
 NOTIF_END_TEXT = "Fine"
-
 HOUR_UNITY_SAMPLES = {"h", "o", "ora", "ore"}
 MIN_UNITY_SAMPLES = {"m", "min", "mins", "minuto", "minuti"}
 
+# Args example: "python pomodoro.py 2 ore"
 quantity = sys.argv[1]
 unity = sys.argv[2]
 
+ # TODO: Add a debug version that prints timer iterations
+
 
 def define_seconds(q, u):
+    """Takes parameters strings and extracts the time value in seconds"""
     
     if(u.lower() in HOUR_UNITY_SAMPLES):
         return q * 3600
@@ -29,6 +34,7 @@ def define_seconds(q, u):
 
 
 def start_session(s):
+    """Starts a pomodoro session"""
     
     while s:
         if(s >= WORK_TIME):
@@ -43,6 +49,7 @@ def start_session(s):
 
 
 def start_timer(s, isWork):
+    """Single timer iteration"""
     
     go_off(isWork)
 
@@ -53,6 +60,7 @@ def start_timer(s, isWork):
 
 
 def go_off(isWork):
+    """Alarm notification"""
 
     if(isWork):
         notification_text = NOTIF_WORK_TEXT
@@ -67,5 +75,4 @@ def stop_session():
     notifier.show_toast(NOTIF_TITLE, NOTIF_END_TEXT)
 
 
-#main
 start_session(define_seconds(int(quantity), unity))
